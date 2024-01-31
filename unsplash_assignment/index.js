bf6f4c8260f365a2ebc94a76f0271bebe58e8b11
Michal <149673103+michalkaszkiel11@users.noreply.github.com>
2024-01-31 01:00:35 +0100

    API load

--- unsplash_assignment/index.js
+++ unsplash_assignment/index.js
@@ -1,29 +1,14 @@
-let data = [];
-const url = "https://api.unsplash.com/";
-const statistics = "/stats/total";
+const accessKey = "Wch0aMrYh-xzLjeB5doI67ADdjjmJ24pw8dtHB7rCj8";
+const apiUrl = "https://api.unsplash.com/photos";
+const millionViews = 1000000;
+const thousandLikes = 1000;
+const country = "US";
 
-const response = await fetch("https://unsplash.com/oauth/token", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-        client_id: "Wch0aMrYh-xzLjeB5doI67ADdjjmJ24pw8dtHB7rCj8",
-        client_secret: "hr98oZfqo2rK5rOWSBJWDL1dN4JiKYl6QTO-gnN0ekU",
-        redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
-        code: "3LGLZy3G2ITA-4y3CzpV2oalIQ5_X-K4QoxQMFd1CLo",
-        grant_type: "authorization_code",
-    }),
-});
-const tokenData = await response.json();
-const accessToken = tokenData.access_token;
-
-const getAcces = async () => {
+async function fetchData(url) {
     try {
-        const response = await fetch(`${url}${statistics}`, {
-            method: "GET",
+        const response = await fetch(url, {
             headers: {
-                Authorization: `Bearer ${accessToken}`,
+                Authorization: `Client-ID ${accessKey}`,
             },
         });
 
@@ -31,16 +16,114 @@ const getAcces = async () => {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
-        const jsonData = await response.json();
-        data = jsonData;
-        console.log(jsonData);
+        const data = await response.json();
+        return data;
     } catch (error) {
         console.error(error);
+        return null;
     }
-};
+}
 
-const startBtn = document.getElementById("getData");
-startBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    getAcces(); // Changed from getData to getAcces
-});
+async function fetchImages(params) {
+    const queryParams = new URLSearchParams(params).toString();
+    const urlWithParams = `${apiUrl}?${queryParams}`;
+    const data = await fetchData(urlWithParams);
+    return data;
+}
+
+async function displayImagesViews(images) {
+    const views = document.getElementById("views");
+    views.innerHTML = "";
+
+    images.forEach((image) => {
+        const imageElement = document.createElement("img");
+        imageElement.src = image.urls.regular;
+        views.appendChild(imageElement);
+        imageElement.classList.add("views");
+    });
+}
+async function displayImagesLikes(images) {
+    const likes = document.getElementById("likes");
+    likes.innerHTML = "";
+
+    images.forEach((image) => {
+        const imageElement = document.createElement("img");
+        imageElement.src = image.urls.regular;
+        likes.appendChild(imageElement);
+        imageElement.classList.add("likes");
+    });
+}
+async function displayByCountry(country) {
+    const countryEl = document.getElementById("country");
+    countryEl.innerHTML = "";
+    country.forEach((country) => {
+        const imageElement = document.createElement("img");
+        imageElement.src = country.urls.regular;
+        countryEl.appendChild(imageElement);
+        countryEl.classList.add("country");
+    });
+}
+
+// Example: Fetch images with over a million views
+const paramsViews = {
+    views: `>${millionViews}`,
+};
+const paramsLikes = {
+    likes: `>${thousandLikes}`,
+};
+const paramsUs = {
+    order_by: `${country}`,
+};
+fetchImages(paramsViews)
+    .then((images) => {
+        if (images) {
+            displayImagesViews(images);
+        }
+    })
+    .catch((error) => console.error(error));
+
+fetchImages(paramsLikes)
+    .then((images) => {
+        if (images) {
+            displayImagesLikes(images);
+        }
+    })
+    .catch((error) => console.log(error));
+
+fetchImages(paramsUs)
+    .then((images) => {
+        if (images) {
+            displayByCountry(images);
+        }
+    })
+    .catch((err) => console.log(err));
+
+// const getData = async () => {
+//     try {
+//         const response = await fetch(
+//             `${url}${path}/?order_by=popular&query=${countryName}`,
+//             {
+//                 method: "GET",
+//                 headers: {
+//                     Authorization: `Client-ID ${clientId}`,
+//                 },
+//             }
+//         );
+
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+
+//         const jsonData = await response.json();
+//         const countryEl = document.getElementById("country");
+//         countryEl.innerHTML = "";
+//         jsonData.forEach((data) => {
+//             const imageEl = document.createElement("img");
+//             imageEl.src = data.urls.regular;
+//             countryEl.appendChild(imageEl);
+//             countryEl.classList.add("country");
+//         });
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
